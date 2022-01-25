c055a1bd3c0c58eead6bb591a181d8cc5058ac9f
PhierBaba <phiermedia@gmail.com>
2022-01-25 06:17:44 +0000

    missing annotation

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -14,7 +14,7 @@ public class HellocdApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Hello to Abanishe, Mosopefoluwa, Eriifeoluwa, Mummy and Daddy -- from phierbaba !";
 	}
 
 	public static void main(String[] args) {
